ba0243380cb2145bf3cbc78212ef2ae8ee4785a5
Gabriel Andrade <99212400+Suntzu1000@users.noreply.github.com>
2023-03-07 10:51:53 -0300

    Adicionando Tokens em Produtos

--- controller/productCtrl.js
+++ controller/productCtrl.js
@@ -1,10 +1,39 @@
 const Product = require("../models/productModel");
 const asyncHandler = require("express-async-handler");
+const slugify = require("slugify");
 
 const createProduct = asyncHandler(async (req, res) => {
   try {
+    if (req.body.title) {
+      req.body.slug = slugify(req.body.title);
+    }
     const newProduct = await Product.create(req.body);
     res.json(newProduct);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+const updateProduct = asyncHandler(async (req, res) => {
+  const id = req.params;
+  try {
+    if (req.body.title) {
+      req.body.slug = slugify(req.body.title);
+    }
+    const updateProduct = await Product.findOneAndUpdate({ id }, req.body, {
+      new: true,
+    });
+    res.json(updateProduct);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+const deleteProduct = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  try {
+    const deleteProduct = await Product.findByIdAndDelete(id);
+    res.json(deleteProduct);
   } catch (error) {
     throw new Error(error);
   }
@@ -14,9 +43,24 @@ const getaProduct = asyncHandler(async (req, res) => {
   const { id } = req.params;
   try {
     const findProduct = await Product.findById(id);
-    res.json(findProduct)
+    res.json(findProduct);
   } catch (error) {
     throw new Error(error);
   }
 });
-module.exports = { createProduct, getaProduct };
+
+const getAllProduct = asyncHandler(async (req, res) => {
+  try {
+    const getallProducts = await Product.find(req.query);
+    res.json(getallProducts);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+module.exports = {
+  createProduct,
+  getaProduct,
+  getAllProduct,
+  updateProduct,
+  deleteProduct,
+};
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,8 +1,18 @@
 const express = require("express");
-const { createProduct, getaProduct } = require("../controller/productCtrl");
+const {
+  createProduct,
+  getaProduct,
+  getAllProduct,
+  updateProduct,
+  deleteProduct,
+} = require("../controller/productCtrl");
+const { isAdmin, authMiddlewares } = require("../middlewares/authMiddlewares");
 const router = express.Router();
 
-router.post("/", createProduct);
+router.post("/", authMiddlewares, isAdmin,  createProduct);
 router.get("/:id", getaProduct);
+router.put("/:id", authMiddlewares, isAdmin,  updateProduct);
+router.delete("/:id", authMiddlewares, isAdmin,  deleteProduct);
+router.get("/", getAllProduct);
 
 module.exports = router;
