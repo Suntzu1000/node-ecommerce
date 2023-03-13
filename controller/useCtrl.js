d0b879acccc69321f82d9e03eb2a746cb3aee660
Gabriel Andrade <99212400+Suntzu1000@users.noreply.github.com>
2023-03-13 11:49:31 -0300

    Autenticação com Email

--- controller/useCtrl.js
+++ controller/useCtrl.js
@@ -198,17 +198,19 @@ const unblockUser = asyncHandler(async (req, res) => {
 
 const updatePassword = asyncHandler(async (req, res) => {
   const { _id } = req.user;
-  const password = req.body;
+  const { password } = req.body;
   validateMongodbId(_id);
   const user = await User.findById(_id);
   if (password) {
     user.password = password;
-    const updatedPassword = await user.save()
-    res.json(updatedPassword)
-  }else {
-    res.json(user)
+    const updatedPassword = await user.save();
+    res.json(updatedPassword);
+  } else {
+    res.json(user);
   }
 });
+
+
 
 module.exports = {
   createUser,
