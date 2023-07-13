a3b7ab749e8491bdf53e7299eeee6f26213a3f04
Gabriel Andrade <99212400+Suntzu1000@users.noreply.github.com>
2023-07-13 17:31:42 -0300

    Movimento CORS

--- index.js
+++ index.js
@@ -21,8 +21,8 @@ const morgan = require("morgan");
 const cors = require("cors");
 dbConnect();
 
-app.use(morgan("dev"));
 app.use(cors());
+app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
