eee5d0d32230e4b9e14b4221d565835e64042bc6
Gabriel Andrade <99212400+Suntzu1000@users.noreply.github.com>
2023-07-13 19:09:38 -0300

    MIGRATE

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,12 +1,16 @@
-const { default: mongoose } = require("mongoose");
+const mongoose = require("mongoose");
 
-const dbConnect = () => {
+const dbConnect = async () => {
   try {
-    mongoose.set('strictQuery', true)
-    const conn = mongoose.connect(process.env.MONGODB_URL);
-    console.log("Banco de dados Conectado!");
+    mongoose.set("strictQuery", true);
+    await mongoose.connect(process.env.MONGODB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Banco de dados conectado!");
   } catch (error) {
-    console.log("Erro em Banco de dados");
+    console.log("Erro em Banco de dados:", error);
   }
 };
+
 module.exports = dbConnect;
